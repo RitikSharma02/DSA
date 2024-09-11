38df69f33860998de08bf73430e8785f2b70b92b
Ritik Sharma <ritiksharma@qurinomsolutions.com>
2024-09-11 10:11:28 +0530

    loops&Conditions

---
+++ src/Conditional_loops/Loops.java
@@ -0,0 +1,43 @@
+package Conditional_loops;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+//        for (int i = 0; i <= 10; i++) {
+//            System.out.println(i);
+//        }
+//        int j=1;
+//        while ( j <= 10){
+//            System.out.println(j);
+//            j+=3;
+//        }
+
+        Scanner in = new Scanner(System.in);
+
+        String Alpha = "";
+
+//        for (int i = 1; i <= num; i++) {
+//
+////            System.out.println(i + " ");
+//            System.out.println("hellow world ");
+//
+//        }
+
+//        int i =1;
+//        while (i<=num){
+//            System.out.println("hellow world");
+//            i+=1;// i++
+//        }
+
+
+while( !Alpha.equalsIgnoreCase("x")){
+    System.out.println();
+    System.out.println("Enter Something, to be precise Enter X to exit hahahaha");
+    Alpha = in.nextLine();
+    System.out.println("You entered: " + Alpha);
+}
+        System.out.println("you are exited sir");
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-
-        System.out.println("hello y all");
-
         Scanner input = new Scanner(System.in);
-        System.out.println(input.next());
-
-    }
-}
+        System.out.println("Enter first number");
+        float num = input.nextFloat();
+        System.out.println("Enter second number");
+        int num2 = input.nextInt();
+        float sum = num+num2;
+        System.out.println(sum);
+}}
 
