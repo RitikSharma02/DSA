46649f1ef09e6bdcac3037c06abbd6bd0c1708f9
Ritik Sharma <pandatritik47@gmail.com>
2024-11-02 12:23:11 +0530

    even odd 02

---
+++ src/first_java_program02/even_odd.java
@@ -0,0 +1,21 @@
+package first_java_program02;
+
+import java.util.Scanner;
+
+public class even_odd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        for (;;) {
+            System.out.println("Enter a number : ");
+            int number = in.nextInt();
+
+            if(number%2==0){
+                System.out.println(number + " is a even number");
+            }
+            else {
+                System.out.println(number + " is a odd number");
+            }
+        }
+    }
+}
